dc5241b092e778df50ad0d3afc087807d171efee
mashannsome <131888090+mashannsome@users.noreply.github.com>
2026-02-07 20:47:11 +0700

    Update telegram_osint.py

--- modules/telegram_osint.py
+++ modules/telegram_osint.py
@@ -2,37 +2,53 @@ from telethon.sync import TelegramClient
 from telethon.tl.functions.contacts import ImportContactsRequest, DeleteContactsRequest
 from telethon.tl.types import InputPhoneContact
 from colorama import Fore, Style
+import time
 
-api_id = 30687090          # isi
-api_hash = "88b84f6d22ef565ba8615a6f4d1cb504"    # isi
+api_id = 30687090
+api_hash = "88b84f6d22ef565ba8615a6f4d1cb504"
+
 
 def telegram_lookup(phone):
     print(Fore.CYAN + "\n[+] Checking Telegram account..." + Style.RESET_ALL)
 
-    with TelegramClient("session", api_id, api_hash) as client:
+    try:
+        with TelegramClient("session", api_id, api_hash) as client:
 
-        contact = InputPhoneContact(
-            client_id=0,
-            phone=phone,
-            first_name="Temp",
-            last_name="OSINT"
-        )
+            contact = InputPhoneContact(
+                client_id=0,
+                phone=phone,
+                first_name="Temp",
+                last_name="OSINT"
+            )
 
-        result = client(ImportContactsRequest([contact]))
+            result = client(ImportContactsRequest([contact]))
 
-        if result.users:
-            user = result.users[0]
+            if result.users:
+                user = result.users[0]
 
-            print(Fore.GREEN + "\n===== TELEGRAM FOUND =====")
-            print("ID        :", user.id)
-            print("Name      :", user.first_name)
-            print("Username  :", user.username)
-            print("Phone     :", phone)
-            print("Bot       :", user.bot)
-            print("==========================\n")
+                data = {
+                    "status": "FOUND",
+                    "id": user.id,
+                    "name": user.first_name,
+                    "username": user.username,
+                    "bot": user.bot
+                }
 
-            # hapus kontak setelah lookup
-            client(DeleteContactsRequest(id=[user.id]))
+                print(Fore.GREEN + "\n===== TELEGRAM FOUND =====")
+                for k, v in data.items():
+                    print(f"{k:<10}: {v}")
+                print("==========================\n")
 
-        else:
-            print(Fore.RED + "\nTidak ditemukan akun Telegram atau privasi disembunyikan\n")
+                # hapus kontak
+                client(DeleteContactsRequest(result.users))
+
+                time.sleep(2)
+                return data
+
+            else:
+                print(Fore.RED + "\nTidak ditemukan akun Telegram atau privasi disembunyikan\n")
+                return {"status": "NOT FOUND"}
+
+    except Exception as e:
+        print(Fore.RED + f"Error: {e}")
+        return {"status": "ERROR"}
